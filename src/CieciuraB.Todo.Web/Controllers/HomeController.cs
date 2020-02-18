cbedb3e21a167aaf0201f47bcd388a0e0984197e
BartoszCieciura <cieciurabartosz01@gmail.com>
2020-02-18 12:43:56 +0100

    dodanie nowej podstrony

--- src/CieciuraB.Todo.Web/Controllers/HomeController.cs
+++ src/CieciuraB.Todo.Web/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace CieciuraB.Todo.Web.Controllers
         {
             return View();
         }
-
+        public IActionResult New()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- src/CieciuraB.Todo.Web/Program.cs
+++ src/CieciuraB.Todo.Web/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -20,6 +22,9 @@ namespace CieciuraB.Todo.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    //webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.UseKestrel();
+                    //webBuilder.UseWebRoot("www");
                     webBuilder.UseStartup<Startup>();
                 });
     }
