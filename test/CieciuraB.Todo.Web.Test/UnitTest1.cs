35be61e0c2e5077010e5ff53c96d67eeaf938151
BartoszCieciura <cieciurabartosz01@gmail.com>
2020-02-18 11:08:53 +0100

    init project

---
+++ test/CieciuraB.Todo.Web.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CieciuraB.Todo.Web.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
